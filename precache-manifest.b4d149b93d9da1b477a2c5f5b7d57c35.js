861248239f0884cd4766d0d8a71e202f4a0b8090
Arun <arun.alagesan@gmail.com>
2020-06-09 14:12:32 +0000

    deploy

---
+++ precache-manifest.b4d149b93d9da1b477a2c5f5b7d57c35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2733864cced8a77fc28",
+    "url": "/my-home-vue-ui/css/app.37765de2.css"
+  },
+  {
+    "revision": "72dc9cd898d095c957592b58da6a1369",
+    "url": "/my-home-vue-ui/index.html"
+  },
+  {
+    "revision": "7364a83279a5e84204e1",
+    "url": "/my-home-vue-ui/js/about.2c8d89c6.js"
+  },
+  {
+    "revision": "d2733864cced8a77fc28",
+    "url": "/my-home-vue-ui/js/app.06d757b4.js"
+  },
+  {
+    "revision": "fef6c19d203ac9c89d12",
+    "url": "/my-home-vue-ui/js/chunk-vendors.192da189.js"
+  },
+  {
+    "revision": "4f51b05917c4fb1af74e8aff964faee1",
+    "url": "/my-home-vue-ui/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-home-vue-ui/robots.txt"
+  }
+]);
